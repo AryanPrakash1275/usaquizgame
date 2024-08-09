fba1958afd10bd08a719e60e8247e16c29d49385
Aryan Prakash <aryanprakash1275@gmail.com>
2024-08-10 01:20:24 +0530

    renamed variables and fixed ending logic

--- usaquizgame.py
+++ usaquizgame.py
@@ -4,14 +4,14 @@ import pandas
 
 screen = turtle.Screen()
 screen.title("USA Game of Guess")
-image = "blank_states_img.gif"
-screen.bgpic(image)
+usa_map = "blank_states_img.gif"
+screen.bgpic(usa_map)
 
-data_states = pandas.read_csv("50_states.csv")
-ans_check_column = data_states.columns[0]
-x_cord= data_states.columns[1]
-y_cord = data_states.columns[2]
-data_states[ans_check_column] = data_states[ans_check_column].astype(str).str.lower()
+parsed_state_csv = pandas.read_csv("50_states.csv")
+states_array = parsed_state_csv.columns[0]
+state_x_cord = parsed_state_csv.columns[1]
+state_y_cord = parsed_state_csv.columns[2]
+parsed_state_csv[states_array] = parsed_state_csv[states_array].astype(str).str.lower()
 
 score = 0
 
@@ -21,15 +21,18 @@ while game_on:
     print(answer)
     if answer == 'exit':
         game_on = False
-    if answer in data_states[ans_check_column].values:
+    if answer in parsed_state_csv[states_array].values:
         print("Yes")
-        row = data_states[data_states[ans_check_column] == answer]
-        x = row[x_cord].values[0]
-        y = row[y_cord].values[0]
+        row = parsed_state_csv[parsed_state_csv[states_array] == answer]
+        x = row[state_x_cord].values[0]
+        y = row[state_y_cord].values[0]
         turtle.penup()
         turtle.goto(int(x), int(y))
         turtle.pendown()
         turtle.dot(10, "red")
         score += 1
+    if len(states_array) == score:
+        game_on = False
+        print("You Win")
     print(score)
 turtle.mainloop()
